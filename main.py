a765910ef0bc6a2f706a9a4cb7ce0f5fa3c90ceb
malinovskiss <edgarsmalinovskis1@gmail.com>
2021-09-21 12:52:08 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#mainīgais saraksts jeb list
+
+#[]
+
+#mainīgais var saturēt dažādus datu tipus
+my_list = ["Teksts", 1, 2]
+print(my_list)
+
+#Elementu skaits mainīgajā
+
+print("'Saraksa my_list elementu skaits: ", len(my_list))
+
+
+#index metode
+
+print(my_list[1])
+
+
+#elementa maiņa
+
+my_list[0] = "Sveiki"
+print(my_list)
+
+#elementu pievienošana
+print(my_list +["Čau"])
