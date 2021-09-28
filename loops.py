5dd16c26751aba64cdf33e69b574248a0372e14d
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2021-09-28 12:56:20 +0300

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,35 @@
+#Edgars Malinovskis
+#28.09.2021
+
+a = [1,2,3,4,5,6,7,8,9,10]
+for i in a:
+
+   print(list)
+
+for i in a:
+    print(i)
+print(17%5)
+for i in a:
+    if i%2 ==0:
+        print(i)
+  #Else
+
+for i in a:
+      if i % 2 ==0:
+          print(i)
+    else:
+        print('nav otrais skaitlis')
+
+#Vārdu saraksta specifiskā izvade
+
+list2 = ['saule','mēness', 'planēta']
+
+for list2[0] in list2:
+    print('Tas ir',list2[0])
+
+    #diapazons/intervāls
+
+    for i in list2 in range(1,2,3):
+        print(i)
+
+
