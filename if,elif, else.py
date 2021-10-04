7344a2c1a4c2f4d892e5e38de4957eb1569056a6
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2021-10-04 12:38:10 +0300

    Add files via upload

---
+++ if,elif, else.py
@@ -0,0 +1,23 @@
+'''
+Edgars Malinovskis
+04.10.2021
+'''
+
+a=14
+if a==14:
+    print (1)
+elif a:
+    print(2)
+else:
+    print(3)
+b=44
+input(b)
+print(b)
+b=input()
+c=input()
+if b==c:
+    print(1)
+elif b == (18):
+    print(2)
+else:
+    print(3)
