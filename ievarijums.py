823e854a7dcd850815bf6ba5203743de5122e11d
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2021-10-05 13:40:48 +0300

    Add files via upload

---
+++ ievarijums.py
@@ -0,0 +1,8 @@
+recepte = {"cukurs":0.6, "kanelis":0.008,"āboli":2.0, "ūdeni":0.2}
+cena = {"cukurs":0.75, "kanelis":0.3, "āboli":0.0, "ūdeni":0.0}
+cukurs= 0.75
+kanelis= 0.3
+āboli= 0.0
+ūdeni= 0.0
+izmaksas= cukurs+kanelis
+print(str(cukurs)+"+"+str(kanelis)+"+"+str(āboli)+"+"+str(ūdeni)+"="+str(izmaksas))
