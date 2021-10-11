002cd70181703faa967aa1ccf792c4703ac0fcb9
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2021-10-11 13:19:47 +0300

    Add files via upload

---
+++ apdruka.py
@@ -0,0 +1,15 @@
+def pasuti_tkreklus(skaits,apdruka,piegade):
+
+#Definē cenas
+
+Cena = ('TEKSTS':5, 'ZĪME':7, 'FOTO':20)
+
+#Aprēķina apdrukas izmaksas
+
+#Definē nosacījumus piegādi un atlaidēm
+piegade = ('TEKSTS':5, True)
+piegade = ('ZĪME':7, True)
+piegade = ('FOTO':20, True)
+
+pasuti_tkreklus(5,'TEKSTS', True)
+
