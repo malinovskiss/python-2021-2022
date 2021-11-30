470a36bbf63664cd418432cfb4ece2bf1939b285
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2021-11-30 13:49:56 +0200

    Add files via upload

---
+++ čempionāts.py
@@ -0,0 +1,19 @@
+def trenins(veidi, daudzums, dienas,):
+    maksa= veidi * daudzums * dienas
+print (trenins(1, 4, 4))
+a = [1,2,3,4,5,6,7,8,9,10]
+for i in a:
+
+   print(list)
+
+for i in a:
+    print(i)
+print(17%5)
+for i in a:
+    if i%2 ==0:
+        print(i)
+for i in a:
+      if i % 2 ==0:
+          print(i)
+else:
+        print('nav otrais skaitlis')
