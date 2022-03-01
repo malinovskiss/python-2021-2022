e580b8d22daba86570fc8d2abb507f5c0d21f80a
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-03-01 13:51:13 +0200

    Update 15.02.py

--- 15.02.py
+++ 15.02.py
@@ -69,3 +69,70 @@ with open("ievaktieDati.json","r", encoding="utf-8") as fails:
 
 with open("ievaktieDati.json","w", encoding="utf-8") as fails:
     json.dump(json_data,fails, indent = 4, ensure_ascii=False)
+    
+    #Datu meklēšana
+
+mekl_vards = input("Ievadi vārdu, kuru vēlies atrast: ")
+
+with open("ievaktieDati.json","r", encoding="utf-8") as fails:
+    json_data = json.load(fails)
+
+for key in json_data.keys():
+    if key == mekl_vards:
+        dati = json_data[key]
+        break
+    else:
+        dati = "Nav sarakstā"
+
+#Savāktie dati jāsaglabā failā ievaktieDati.json
+
+with open("ievaktieDati.json","w", encoding="utf-8") as fails:
+    json.dump(json_data,fails, indent = 4, ensure_ascii=False)
+
+while True:
+    print("\nIzvēlies darbību:")
+    print("1 - kontakta pievienošana")
+    print("2 - kontakta meklēšana")
+
+    ievad_dati = (
+        "Uzvārds":uzvards,
+        "Vecums":vecums,
+        "Telefona numurs":tel_nr
+    )
+
+    kont_saglabat(vards,ievad_dati)
+
+def kont_saglabat(vards,ievad_dati):
+    with open("ievaktieDati.json","r", encoding="utf-8") as fails:
+        json_data = json.load(fails)
+
+    ir_saraksta = False
+
+    izveele = input()
+
+    if izveele =="1":
+        kont_piev()
+    elif int(izveele) == 2:
+        kont_mekl()
+    elif int(izveele) == 3:
+        exit()
+    else:
+        print("\nIzvēlies kādu no darbībām!")
+
+import datetime
+
+print(datetime.datetime.now())
+
+print(datetime.datetime(2020,7,12))
+
+"""
+%A - diena
+%d - mēneša diena
+%j - diena gadā
+%B - mēnesis
+"""
+
+a = datetime.datetime.now()
+parv_laiks = a.isoformat()
+json_a = json.dump(a)
+print(json_a)
