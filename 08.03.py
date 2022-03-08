6d1ce3dffe92919550967110e36c913ccbb1e78a
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-03-08 13:49:17 +0200

    Update 08.03.py

--- 08.03.py
+++ 08.03.py
@@ -1,3 +1,4 @@
+from itertools import combinations_with_replacement
 import sqlite3
 
 db = sqlite3.connect("test.db")
@@ -26,8 +27,8 @@ db.execute("""INSERT INTO edienkarte
             VALUES (1,"makaroni",1.5,"glutēns")
 """)
 
-db.commit()
 
+db.commit()
 
 
 dati = db.execute("SELECT* FROM edienkarte WHERE cena > 0.5")
@@ -41,5 +42,23 @@ db.execute ('''CREATE TABLE IF NOT EXISTS sastavdalas
 #    OTRA    CHAR(50),
 #    TRESA   CHAR(50)
 #    )''')
+
+pirma = input("Pirmā sastāvdaļa: ")
+otra = input("Otrā sastāvdaļa: ")
+tresa = input("Trešā sastāvdaļa: ")
+
+db.execute("""INSERT INTO sastavdalas
+            (pirma,otra,tresa)
+            VALUES (:pirma,:otra,:tresa)
+""",{"pirma":"pirma","otra":"otra","tresa":"tresa"})
+
+db.commit()
+
+dati = db.execute("SELECT * FROM edienkarte")
+for rinda in dati:
+    print("ID:", rinda[0])
+    print("Nosaukums:", rinda[1])
+    print("Cena:", rinda[2])
+    print("Alergēni:", rinda[3], "/n")
 
 db.close()
