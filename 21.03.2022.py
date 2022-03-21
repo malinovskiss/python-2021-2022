28e647961c4f71e0b6a9acbb63cee9c616ffb479
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-03-21 13:51:11 +0200

    Add files via upload

---
+++ 21.03.2022.py
@@ -0,0 +1,21 @@
+import random
+
+masivs = ['a','b','c']
+a = ("Akmens")
+b = ("Šķēres")
+c = ("Papīrs")
+
+izveele = random.choice(masivs)
+
+
+name_list = ['Akmens', 'Šķēres', 'Papīrs']
+
+print(random.choice(name_list))
+
+from random import choice
+
+def random_element():
+   return choice() 
+
+print (random_element(['Akmens', 'Šķēres', 'Papīrs']))
+
