b4454388134239460f329f2d758eb8e3b8ea3be3
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-03-22 12:18:52 +0200

    Update 21.03.2022.py

--- 21.03.2022.py
+++ 21.03.2022.py
@@ -1,4 +1,6 @@
 import random
+import json
+import sqlite3
 
 masivs = ['a','b','c']
 a = ("Akmens")
@@ -17,5 +19,38 @@ from random import choice
 def random_element():
    return choice() 
 
-print (random_element(['Akmens', 'Šķēres', 'Papīrs']))
+print(random_element(['Akmens', 'Šķēres', 'Papīrs']))
 
+while True:
+
+    vecums = input("Ievadi savu vecumu:")
+    if vecums.isdigit() == True:
+        continue
+    else:
+        break
+
+print(vecums)
+
+
+while True:
+    vards = input("Ievadi savu vārdu:")
+
+    if vards.strip() == "":
+        print(vards)
+        continue
+    else:
+        break
+
+print(vards)
+
+with open("ievaktieDati.json","r", encoding="utf-8") as fails:
+    json_data = json.load(fails)
+
+db = sqlite3.connect("test.db")
+
+dati = db.execute("Vecums * FROM Vārds")
+for rinda in dati:
+    print("Vecums:", rinda[0])
+    print("Vārds:", rinda[1], "/n")
+
+db.close()
