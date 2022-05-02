726977a6fa568739682dfd9451993d5888a8868d
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-05-02 13:51:11 +0300

    Update try_except_else.py

--- try_except_else.py
+++ try_except_else.py
@@ -29,3 +29,20 @@ askint()
 
 for i in ['a', 'b', 'c']:
     print(i**2)
+    # #askint()
+
+
+    #02.05.
+
+    import sys
+
+    saraksts = ['a',0,2]
+
+for i in saraksts:
+    try:
+        print("Skaitlis ir:",i)
+        a = 1/int(i)
+        break
+    except:
+        print("Kļūda:",sys.exc_info()[0])
+print(a)
