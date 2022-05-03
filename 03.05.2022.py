5c7360c244986b3db8619ba0d13335daa0f47261
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-05-03 12:41:57 +0300

    Update 03.05.2022.py

--- 03.05.2022.py
+++ 03.05.2022.py
@@ -1,5 +1,16 @@
 stop = True
-while stop : True
-diena = input("pirmdiena")
-skaitlis = input("44")
-print(skaitlis)
+while stop == True:
+    try:
+        laiks = int(input("ievadi laiku(0-23): "))
+        if laiks >= 0 and laiks <= 23:
+            stop = True
+        else:
+            print("mēģini vēlreiz")
+    elif laiks >= 5 and laiks <=11:
+    print("labrīt!")
+    elif laiks >= 12 and laiks <= 17:
+    print("labdien!")
+    elif laiks >= 18 and laiks <=20:
+    print("labvakar!")
+    elif laiks >= 21 and laiks <=23:
+    print("Uzredzēšanos")
